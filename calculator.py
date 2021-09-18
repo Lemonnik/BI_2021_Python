5ef4e903696f8fb1886160f5692ae38a26754a58
Lemonnik <ds.traktirov@gmail.com>
2021-09-18 10:18:28 +0300

    type check for inputs moved to a separate function (check_num)

--- calculator.py
+++ calculator.py
@@ -1,37 +1,46 @@
 def calculate(num1, num2, operator):
-    try:
-        _ = float(num1)
-        _ = float(num2)
-    except:
-        return 'Input must be int/float'
-
-    if operator not in ['+', '-', '*', '/']:
-        return 'Only +-*/ are supported'
-
-    # если с инпутом всё ок
     return eval(num1 + operator + num2)
+
+
+def check_num(num):
+    try:
+        float(num)
+    except:
+        return False
+    return True
 
 
 def main():
     while True:
+        # число 1
         num1 = input()
         if num1 == 'exit':  # не придумал ничего лучше
             break
+        elif not check_num(num1):
+            print('Please input number')
+            continue
 
+        # оператор
         operator = input()
         if operator == 'exit':
             break
+        elif operator not in ['+', '-', '*', '/']:
+            print('Only +-*/ are supported')
+            continue
 
+        # число 2
         num2 = input()
         if num2 == 'exit':
             break
+        elif not check_num(num2):
+            print('Please input number')
+            continue
 
-        print(calculate(num1, num2, operator))
+        # результат
+        result = calculate(num1, num2, operator)
+        print(result)
 
 
 
 if __name__=='__main__':
     main()
-
-
-# print(type(float('55.5')))
