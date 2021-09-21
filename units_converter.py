d7c46dfa023663f0d7ea1d96f99ee61fc0bbbb69
Lemonnik <ds.traktirov@gmail.com>
2021-09-21 21:08:06 +0300

    convertation bar -> Pa corrected (w/o mistake)

--- units_converter.py
+++ units_converter.py
@@ -27,7 +27,7 @@ def unit_conv(unit_from, unit_to, value):
                             'atm': 9.8692e-6
                             },
                     'bar': {
-                            'Pa': 100000,
+                            'Pa': 100000.0,
                             'atm': 0.986923
                             },
                     'atm': {
